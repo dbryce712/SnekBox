b6bd90107f35caac21024d8fc2dc151b964261e4
Bryce <dubryce@outlook.com>
2024-09-29 04:21:10 +0000

    ROA mode basic changes

--- src/modes/RivalsOfAether.cpp
+++ src/modes/RivalsOfAether.cpp
@@ -20,22 +20,13 @@ void RivalsOfAether::UpdateDigitalOutputs(InputState &inputs, OutputState &outpu
     outputs.b = inputs.b;
     outputs.x = inputs.x;
     outputs.y = inputs.y;
+    outputs.buttonL = inputs.lightshield;
     outputs.buttonR = inputs.z;
-    if (inputs.nunchuk_connected) {
-        // Lightshield with C button.
-        if (inputs.nunchuk_c) {
-            outputs.triggerLAnalog = 49;
-        }
-        outputs.triggerLDigital = inputs.nunchuk_z;
-    } else {
-        outputs.triggerLDigital = inputs.l;
-    }
+    outputs.triggerLDigital = inputs.l;
     outputs.triggerRDigital = inputs.r;
     outputs.start = inputs.start;
     outputs.select = inputs.select;
     outputs.home = inputs.home;
-    outputs.leftStickClick = inputs.lightshield;
-    outputs.rightStickClick = inputs.midshield;
 
     // Activate D-Pad layer by holding Mod X + Mod Y.
     if (inputs.mod_x && inputs.mod_y) {
@@ -44,6 +35,16 @@ void RivalsOfAether::UpdateDigitalOutputs(InputState &inputs, OutputState &outpu
         outputs.dpadLeft = inputs.c_left;
         outputs.dpadRight = inputs.c_right;
     }
+
+    // Activate select by holding Mod X
+    if ((inputs.mod_x)) outputs.select = inputs.start;
+
+    // Activate home by holding Mod Y
+    if ((inputs.mod_y)) outputs.home = inputs.start;
+
+    // Don't override dpad up if it's already pressed using the MX + MY dpad
+    // layer.
+    outputs.dpadUp = outputs.dpadUp || inputs.midshield;
 }
 
 void RivalsOfAether::UpdateAnalogOutputs(InputState &inputs, OutputState &outputs) {
@@ -156,6 +157,11 @@ void RivalsOfAether::UpdateAnalogOutputs(InputState &inputs, OutputState &output
     if (inputs.mod_x && inputs.mod_y) {
         outputs.rightStickX = 128;
         outputs.rightStickY = 128;
+    }
+
+    // Turns off Start when holding Mod X or Mod Y
+    if ((inputs.mod_x || inputs.mod_y)) {
+        outputs.start = false;
     }
 
     // Nunchuk overrides left stick.
