7b2c2b052517c28b13924734d1c1b2e38783b4b5
dbryce712 <dubryce@outlook.com>
2025-04-07 14:13:26 -0400

    Changes to DACUS/turnaround vertical special code

--- src/modes/ProjectM.cpp
+++ src/modes/ProjectM.cpp
@@ -88,13 +88,14 @@ void ProjectM::UpdateAnalogOutputs(const InputState &inputs, OutputState &output
     bool shield_button_pressed = inputs.lf4 || inputs.rf5;
 
     if (directions.diagonal) {
-        if (directions.y == 1) {
+        // Up/Down smash without letting go of forward
+        if (inputs.rt1 || inputs.rf3) {
             outputs.leftStickX = 128 + (directions.x * 83);
             outputs.leftStickY = 128 + (directions.y * 93);
         }
 
-        // down + horizontal + B outputs down special
-        if (inputs.rf1 && directions.y == -1) {
+        // 45 + B outputs vertical special when grounded
+        if (inputs.rf1) {
             outputs.leftStickX = 128 + (directions.x * 28);
             outputs.leftStickY = 128 + (directions.y * 85);
         }
