a234fa2621edfe98546204809d9156e60f38a868
dbryce712 <dubryce@outlook.com>
2025-04-09 17:36:12 -0400

    Added shield pressed condition to Mod Y diagonal shield drop code, .gitignore

--- src/modes/ProjectM.cpp
+++ src/modes/ProjectM.cpp
@@ -177,7 +177,7 @@ void ProjectM::UpdateAnalogOutputs(const InputState &inputs, OutputState &output
             outputs.leftStickX = 128 + (directions.x * 28);
             outputs.leftStickY = 128 + (directions.y * 58);
 
-            if (directions.y == -1) {
+            if (shield_button_pressed && directions.y == -1) {
                 outputs.leftStickX = 128 + (directions.x * 28);
                 outputs.leftStickY = 128 + (directions.y * 69);
             }
