f9b3d1b0ef60226138424b6dbb4bd605ce33a626
dbryce712 <dubryce@outlook.com>
2025-05-27 17:27:27 -0400

    Keyboard mode adjustments

--- src/modes/DefaultKeyboardMode.cpp
+++ src/modes/DefaultKeyboardMode.cpp
@@ -6,26 +6,14 @@
 DefaultKeyboardMode::DefaultKeyboardMode() : KeyboardMode() {}
 
 void DefaultKeyboardMode::UpdateKeys(const InputState &inputs) {
-    Press(HID_KEY_A, inputs.lf4);
-    Press(HID_KEY_B, inputs.lf3);
-    Press(HID_KEY_C, inputs.lf2);
-    Press(HID_KEY_D, inputs.lf1);
-    Press(HID_KEY_E, inputs.lt1);
-    Press(HID_KEY_F, inputs.lt2);
-    Press(HID_KEY_G, inputs.mb3);
-    Press(HID_KEY_H, inputs.mb1);
-    Press(HID_KEY_I, inputs.mb2);
-    Press(HID_KEY_J, inputs.rf5);
-    Press(HID_KEY_K, inputs.rf6);
-    Press(HID_KEY_L, inputs.rf7);
-    Press(HID_KEY_M, inputs.rf8);
-    Press(HID_KEY_N, inputs.rf1);
-    Press(HID_KEY_O, inputs.rf2);
-    Press(HID_KEY_P, inputs.rf3);
-    Press(HID_KEY_Q, inputs.rf4);
-    Press(HID_KEY_R, inputs.rt4);
-    Press(HID_KEY_S, inputs.rt3);
-    Press(HID_KEY_T, inputs.rt5);
-    Press(HID_KEY_U, inputs.rt1);
-    Press(HID_KEY_V, inputs.rt2);
+    Press(HID_KEY_SHIFT_LEFT, inputs.rt2);
+    Press(HID_KEY_ARROW_LEFT, inputs.lf3);
+    Press(HID_KEY_ARROW_DOWN, inputs.lf2);
+    Press(HID_KEY_ARROW_RIGHT, inputs.lf1);
+    Press(HID_KEY_ARROW_UP, inputs.lf5);
+    Press(HID_KEY_ENTER, inputs.mb1);
+    Press(HID_KEY_SPACE, inputs.lt1);
+    Press(HID_KEY_X, inputs.rf1);
+    Press(HID_KEY_Z, inputs.rt1);
+    Press(HID_KEY_C, inputs.rf3);
 }
